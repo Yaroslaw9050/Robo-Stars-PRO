95a8746ec59e1362718b8cd6afe9a5daf056b074
Yaroslaw9050 <ywgk0102@gmail.com>
2022-05-19 20:35:52 +0300

    Add OutBattle button and work script. Fix RommManager. Add GameManager

---
+++ Assets/Script/Level/GameNetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class GameNetworkManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject allPlayerUI;
+    private PhotonView pv;
+
+    private void Awake()
+    {
+        pv = gameObject.GetPhotonView();
+    }
+    private void Start()
+    {
+        if(!pv.IsMine) 
+        {
+            Destroy(allPlayerUI);
+            return;
+        }
+        PhotonNetwork.Instantiate(Path.Combine("Player"), Vector3.zero, Quaternion.identity); 
+    }
+    public void OutOfBattle()
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+        PhotonNetwork.LeaveRoom(); 
+        SceneManager.LoadScene(0);
+    }
+    
+}
